fa797335c3bf7be82fa5e9f86628cf9d3489cd3c
heronet <sirat4757@gmail.com>
2021-11-18 18:19:21 +0600

    AddedCors

--- CompetitionAPI/Program.cs
+++ CompetitionAPI/Program.cs
@@ -24,6 +24,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
+app.UseCors(policy =>
+{
+    policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+});
+
 app.UseAuthentication();
 
 app.UseAuthorization();
